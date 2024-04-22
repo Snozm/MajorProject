cf5b21a190fd7c92184aebc38168699f48e9fde3
v.boychev24 <v.boychev24@acsbg.org>
2024-04-22 12:49:19 +0300

    ok

--- MajorProject/Form 2.Designer.cs
+++ MajorProject/Form 2.Designer.cs
@@ -30,11 +30,15 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_2));
             dataGridView1 = new DataGridView();
-            Column_TextBox = new TextBox();
+            ColumnTextBox = new TextBox();
             Filter_TextBox = new TextBox();
             label1 = new Label();
             label2 = new Label();
             pictureBox1 = new PictureBox();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
@@ -46,20 +50,20 @@
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 62;
             dataGridView1.RowTemplate.Height = 33;
-            dataGridView1.Size = new Size(993, 885);
+            dataGridView1.Size = new Size(1058, 885);
             dataGridView1.TabIndex = 0;
             // 
-            // Column_TextBox
+            // ColumnTextBox
             // 
-            Column_TextBox.Location = new Point(64, 184);
-            Column_TextBox.Name = "Column_TextBox";
-            Column_TextBox.Size = new Size(417, 31);
-            Column_TextBox.TabIndex = 1;
-            Column_TextBox.TextChanged += textBox1_TextChanged;
+            ColumnTextBox.Location = new Point(46, 193);
+            ColumnTextBox.Name = "ColumnTextBox";
+            ColumnTextBox.Size = new Size(262, 31);
+            ColumnTextBox.TabIndex = 1;
+            ColumnTextBox.TextChanged += textBox1_TextChanged;
             // 
             // Filter_TextBox
             // 
-            Filter_TextBox.Location = new Point(549, 184);
+            Filter_TextBox.Location = new Point(858, 193);
             Filter_TextBox.Name = "Filter_TextBox";
             Filter_TextBox.Size = new Size(246, 31);
             Filter_TextBox.TabIndex = 2;
@@ -67,27 +71,27 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(102, 133);
+            label1.Location = new Point(46, 156);
             label1.Name = "label1";
-            label1.Size = new Size(136, 25);
+            label1.Size = new Size(126, 25);
             label1.TabIndex = 3;
-            label1.Text = "Choose column";
+            label1.Text = "Departure City";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(581, 133);
+            label2.Location = new Point(858, 156);
             label2.Name = "label2";
-            label2.Size = new Size(107, 25);
+            label2.Size = new Size(94, 25);
             label2.TabIndex = 4;
-            label2.Text = "Choose row";
+            label2.Text = "TicketType";
             label2.Click += label2_Click;
             // 
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.x2sxnv1ogieb1__1_;
             pictureBox1.InitialImage = (Image)resources.GetObject("pictureBox1.InitialImage");
-            pictureBox1.Location = new Point(1074, 243);
+            pictureBox1.Location = new Point(1553, 225);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(916, 885);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
@@ -95,17 +99,54 @@
             pictureBox1.TabStop = false;
             pictureBox1.Click += pictureBox1_Click;
             // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(346, 193);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(285, 31);
+            textBox1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(346, 156);
+            label3.Name = "label3";
+            label3.Size = new Size(62, 25);
+            label3.TabIndex = 7;
+            label3.Text = "Arrival";
+            label3.Click += label3_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(647, 193);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(205, 31);
+            textBox2.TabIndex = 8;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(657, 155);
+            label4.Name = "label4";
+            label4.Size = new Size(48, 25);
+            label4.TabIndex = 9;
+            label4.Text = "Train";
+            // 
             // Form_2
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.LightCyan;
-            ClientSize = new Size(2031, 1149);
+            ClientSize = new Size(2500, 1149);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
             Controls.Add(pictureBox1);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(Filter_TextBox);
-            Controls.Add(Column_TextBox);
+            Controls.Add(ColumnTextBox);
             Controls.Add(dataGridView1);
             Name = "Form_2";
             Text = "Form_2";
@@ -118,10 +159,14 @@
         #endregion
 
         private DataGridView dataGridView1;
-        private TextBox Column_TextBox;
+        private TextBox ColumnTextBox;
         private TextBox Filter_TextBox;
         private Label label1;
         private Label label2;
         private PictureBox pictureBox1;
+        private TextBox textBox1;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label4;
     }
 }
